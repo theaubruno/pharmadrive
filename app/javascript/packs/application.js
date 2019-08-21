fd24de663c31e798669ace68e467c3575a8ec298
Vincent Grenier <vincentgrenier@hotmail.fr>
2019-08-21 14:57:24 +0200

    Enabled the JS for the form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+
+const form = document.getElementById('newordo')
+const button = document.getElementById('showfrom')
+
+button.addEventListener("focus", (event) => {
+  form.classList.toggle("d-none")
+});
+
