cf2c002fda0731ae8e31a49505b7304098096431
Vincent Grenier <vincentgrenier@hotmail.fr>
2019-08-21 15:03:20 +0200

    Enabled the JS for the form v2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,10 @@
 import "bootstrap";
 
+if (document.getElementById('newordo')!== null) {
 const form = document.getElementById('newordo')
 const button = document.getElementById('showfrom')
 
 button.addEventListener("focus", (event) => {
   form.classList.toggle("d-none")
 });
-
+};
