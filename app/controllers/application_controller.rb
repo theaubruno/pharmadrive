548c9d02d8db8bba2f1a7bec89cb218f8c8a9918
Louis Loison <lw.loison@gmail.com>
2019-08-21 12:05:27 +0200

    front login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    doctor_patients_path(current_user) #your path
+    doctor_patients_path(current_user)
   end
 end
