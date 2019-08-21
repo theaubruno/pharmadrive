04b681a85128edee208d98e3e8bf25176c49c4a2
Théau Bruno <bn.theau@gmail.com>
2019-08-21 23:52:21 +0200

    ajout seed + creation partie pharmacien

---
+++ app/controllers/pharmacy/lists_controller.rb
@@ -0,0 +1,35 @@
+class Pharmacy::ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @list.update(list_params)
+
+    redirect_to pharmacy_lists_path(@pharmacy)
+  end
+
+  def destroy
+    @list.destroy
+
+    redirect_to pharmacy_lists_path(@pharmacy)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:patient_id, :prescribed_at, :user_id, :delivered)
+  end
+end
