afd90b239a481bde9b446b81ac6571c593164a30
Louis Loison <lw.loison@gmail.com>
2019-08-23 13:04:54 +0200

    nouvelles infos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,3 +8,33 @@ def sommeordos(array)
     return sum
   end
 end
+
+def sommelists(array)
+  lists = []
+  array.each do |list|
+    if list.delivered? && list.delivered_at.day == Time.now.day
+      lists << list
+    end
+  end
+  return lists
+end
+
+def sommelists2(array)
+  lists2 = []
+  array.each do |list|
+    if list.delivered?
+      lists2 << list
+    end
+  end
+  return lists2
+end
+
+def tempsmoyen(array)
+  times = []
+  array.each do |list|
+    time = (list.ready_at.to_i - list.created_at.to_i)
+    times << time
+  end
+  average = times.inject { |sum, el| sum + el }.to_f / times.size
+  return average
+end
