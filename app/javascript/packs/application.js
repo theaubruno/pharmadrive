1f6ca24e2d232409de1812032fac9c211e02a678
Théau Bruno <bn.theau@gmail.com>
2019-08-29 10:41:57 +0200

    pdf + js typed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -164,6 +164,6 @@ import { initMapbox2 } from '../plugins/init_mapbox2';
 //= require sweetalert2
 //= require sweet-alert2-rails
 import { typedHome } from '../components/typed';
-if (window.location.href === "https://pharmadrive.herokuapp.com" || window.location.href === "http://localhost:3000/" ){
+if (window.location.href === "https://pharmadrive.herokuapp.com/" || window.location.href === "http://localhost:3000/" ){
  typedHome();
 };
